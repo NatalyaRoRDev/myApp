1d5e96c8955bdab98e8b213b69329849f5051a51
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-01-25 12:02:20 +0300

    Added Static Pages home, help, contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-root to: "home#index"
-  
+  root to: "static_pages#home"
+
+  get 'help' => 'static_pages#help'
+  get 'contacts' => 'static_pages#contacts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
